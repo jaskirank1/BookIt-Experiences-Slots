2c830176fddee7bbb00afeb853eecf90a8f7ba7b
Jaskiran <kiran18202@gmail.com>
2025-10-30 19:37:36 +0530

    changes

--- frontend/src/pages/BookingConfirmationPage.tsx
+++ frontend/src/pages/BookingConfirmationPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import tickIcon from "../assets/tickMark.png";
 
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ExperiencesList from "../components/ExperiencesList";
 
 const Homepage = () => {
